a78f0f346f0bcffac3358cd0a873aec999b9188e
dparbhakar <6168102+dparbhakar@users.noreply.github.com>
2022-10-07 14:17:49 +0200

    cli/workspace: Updated open command documentation

--- cli/src/commands/workspace/open.ts
+++ cli/src/commands/workspace/open.ts
@@ -9,11 +9,11 @@ export default class WorkspaceOpen extends BaseCommand {
   static description = "Quickly open important URLs from your cluster.";
 
   static examples = [`
-    $ kube-core workspace open # Opens first host on every ingress cluster-wide, matching: -l workspace.kube-core.io/name=default
-    $ kube-core workspace open logging # Opens first host on every ingress cluster-wide, matching: -l workspace.kube-core.io/name=logging
+    $ kube-core workspace open # Opens first host on every ingress cluster-wide matching: -l workspace.kube-core.io/name=default
+    $ kube-core workspace open logging # Opens first host on every ingress cluster-wide matching: -l workspace.kube-core.io/name=logging
     $ kube-core workspace open all # Opens first host on every ingress cluster-wide
-    $ kube-core workspace open -n integration # Opens first host on every ingress, in integration Namespace
-    $ kube-core workspace open -n integration admin # Opens first host on every ingress, in integration Namespace, matching: -l workspace.kube-core.io/name=admin
+    $ kube-core workspace open all -n integration  # Opens first host on every ingress in integration Namespace
+    $ kube-core workspace open admin -n integration  # Opens first host on every ingress in integration Namespace matching: -l workspace.kube-core.io/name=admin
   `];
 
   static args = [];
