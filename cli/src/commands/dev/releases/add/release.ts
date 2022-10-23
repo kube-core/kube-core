c85423bd3c3849c00c6c9068054b474f6039a362
dparbhakar <6168102+dparbhakar@users.noreply.github.com>
2022-10-24 00:47:54 +0200

    cli/commands: Updated some docs

--- cli/src/commands/dev/releases/add/release.ts
+++ cli/src/commands/dev/releases/add/release.ts
@@ -7,7 +7,7 @@ import BaseCommand from "../../../../base";
 export default class DevReleasesAddRelease extends BaseCommand {
   static description = "Adds a Helmfile Release to kube-core.";
 
-  static examples = [`$ kube-core dev:releases:add:release chart-name release-name namespace `];
+  static examples = [`$ kube-core dev releases add release chart-name release-name namespace`];
 
   static flags = {};
 
@@ -18,27 +18,5 @@ export default class DevReleasesAddRelease extends BaseCommand {
   async run(): Promise<void> {
     const { args, argv, flags } = await this.parse(DevReleasesAddRelease);
     await this.utils.runCoreScript("commands/dev/releases/add-release.sh", argv);
-
-
-    // Not working
-    // const plopfile = path.join(__dirname, 'plopfile.mjs')
-    // const plop = await nodePlop(plopfile);
-    // Plop.prepare({
-    //     cwd: process.env.cwd,
-    //     configPath: path.join(__dirname, 'plopfile.mjs'),
-    //     preload: [],
-    //     completion: ""
-    // }, env => 
-    //     Plop.execute(env, (env) => {
-    //         const options = {
-    //             ...env,
-    //             dest: process.cwd() // this will make the destination path to be based on the cwd when calling the wrapper
-    //         }
-    //         return run(options, undefined, true)
-    //     })
-    // )
-
-      
   }
-
 }
