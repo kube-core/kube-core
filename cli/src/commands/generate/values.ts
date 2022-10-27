248637fb924e372026a34e9a85660b4acca587b6
dparbhakar <6168102+dparbhakar@users.noreply.github.com>
2022-10-27 18:54:39 +0200

    cli/commands: Updated generate values to allow merge from core over local

--- cli/src/commands/generate/values.ts
+++ cli/src/commands/generate/values.ts
@@ -1,6 +1,9 @@
 import { Command, Flags } from "@oclif/core";
 import BaseCommand from "../../base";
-
+import upath from "upath";
+import YAML from 'yaml'
+import * as fs from 'fs-extra'
+import merge from 'lodash.merge'
 export default class GenerateValues extends BaseCommand {
   static description =
     "Work in progress feature. Generates values files for a consumer cluster.";
@@ -13,6 +16,51 @@ export default class GenerateValues extends BaseCommand {
 
   async run(): Promise<void> {
     const { args, argv, flags } = await this.parse(GenerateValues);
-    await this.utils.runCoreScript("commands/generate/values.sh", argv);
+    // await this.utils.runCoreScript("commands/generate/values.sh", argv);
+
+    // Getting layers from core
+    let releaseFiles = await this.utils.getFilesAsList(upath.join(this.corePath, "core/layers/releases"))
+    let configFiles = await this.utils.getFilesAsList(upath.join(this.corePath, "core/layers/config"))
+    // console.log(files)
+
+    const localConfigDir = upath.join(this.clusterConfigDirPath, "values")
+
+    for(let file of configFiles) {
+      let {name, ext} = upath.parse(file)
+      let targetPath = upath.join(localConfigDir, "core/config", `${name}${ext}`)
+      let coreFileData = YAML.parse((await fs.readFile(file, "utf8")))
+      let mergedData = {}
+
+      if (this.utils.fileExists(targetPath)) {
+        // console.log(`File exists: ${targetPath}`)
+        let currentFileData = YAML.parse((await fs.readFile(targetPath, "utf8")))
+        console.info(`Merging: kube-core:${file.replace(this.corePath, '')}\t<- local:${targetPath.replace(this.clusterConfigDirPath, '')}`)
+        mergedData = merge(coreFileData, currentFileData)
+        // console.log(mergedData)
+      } else {
+        mergedData = coreFileData
+      }
+      await this.utils.mkdir(upath.parse(targetPath).dir)
+      await fs.writeFile(targetPath, YAML.stringify(mergedData))
+    }
+
+
+    for(let file of releaseFiles) {
+      let {name, ext} = upath.parse(file)
+      let targetPath = upath.join(localConfigDir, "core/releases", `${name}${ext}`)
+      let coreFileData = YAML.parse((await fs.readFile(file, "utf8")))
+      let mergedData = {}
+
+      if (this.utils.fileExists(targetPath)) {
+        let currentFileData = YAML.parse((await fs.readFile(targetPath, "utf8")))
+        mergedData = merge(coreFileData, currentFileData)
+        // console.log(mergedData)
+      } else {
+        mergedData = coreFileData
+      }
+      await this.utils.mkdir(upath.parse(targetPath).dir)
+      await fs.writeFile(targetPath, YAML.stringify(mergedData))
+    }
+
   }
 }
