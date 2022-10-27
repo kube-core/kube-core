0c95c048f799905ffa99bb7583f7ac8358ce2f94
dparbhakar <6168102+dparbhakar@users.noreply.github.com>
2022-10-27 18:36:58 +0200

    cli/base: Migrated some common logic in base command class

--- cli/src/base.ts
+++ cli/src/base.ts
@@ -1,18 +1,22 @@
 import { Command, Flags } from "@oclif/core";
-import path from "path";
+import upath from "upath";
 import yaml from "js-yaml";
 import * as utils from "./lib/utils";
 import findUp from 'find-up';
 import * as fs from 'fs-extra'
+import merge from 'lodash.merge'
+const { lookpath } = require('lookpath');
 
 
 export default abstract class extends Command {
   public utils;
   public scriptsPath;
+  public corePath;
   public clusterContext
   public clusterConfig
   public clusterConfigPath
   public clusterConfigDirPath
+  public gitopsConfigHasChanges
 
   static flags = {
     // name: Flags.string({
@@ -37,16 +41,27 @@ export default abstract class extends Command {
 
 
   }
+  async checkRequirements() {
+    const gron = await lookpath('gron');
+    if (gron === undefined) {
+      console.error('Please install gron on your system and make it available to your path. https://github.com/tomnomnom/gron')
+      process.exit(0)
+    }
+  }
 
   async init() {
     this.utils = utils;
-    this.scriptsPath = `${path.resolve(`${require.main.filename}/../../scripts`)}`;
-    let scriptPath = `${path.join(this.scriptsPath, "src/utils/read-core-path.sh")}`;
-    let defaultClusterConfigPath = path.join(this.scriptsPath, "default-cluster-config.yaml")
+
+    await this.checkRequirements()
+
+    this.scriptsPath = `${upath.resolve(`${require.main.filename}/../../scripts`)}`;
+    this.corePath = `${upath.resolve(`${this.scriptsPath}/../../`)}`;
+    let scriptPath = `${upath.join(this.scriptsPath, "src/utils/read-core-path.sh")}`;
+    let defaultClusterConfigPath = upath.join(this.scriptsPath, "default-cluster-config.yaml")
     let file = 'cluster-config.yaml'
 
     // let corePath = await this.utils.cliQuiet(scriptPath);
-    // let clusterConfigPath = `${path.join(this.corePath, "src/utils/read-core-path.sh")}`
+    // let clusterConfigPath = `${upath.join(this.corePath, "src/utils/read-core-path.sh")}`
 
     const foundPath = await findUp(file)
     if (!foundPath) {
@@ -56,7 +71,7 @@ export default abstract class extends Command {
 
       this.clusterContext = true
       let clusterConfigPath = foundPath
-      let clusterConfigDirPath = path.dirname(clusterConfigPath)
+      let clusterConfigDirPath = upath.dirname(clusterConfigPath)
 
       // console.log(clusterConfigPath)
       // console.log(clusterConfigDirPath)
@@ -64,17 +79,22 @@ export default abstract class extends Command {
       const clusterConfig = yaml.load((await fs.readFile(clusterConfigPath, "utf8")))
       const defaultClusterConfig = yaml.load((await fs.readFile(defaultClusterConfigPath, "utf8")))
 
-      const data = {...defaultClusterConfig, ...clusterConfig}
+      const data = merge(defaultClusterConfig, clusterConfig)
       // console.log(clusterConfig.cluster.config.context)
       this.clusterConfig = data
       this.clusterConfigPath = clusterConfigPath
       this.clusterConfigDirPath = clusterConfigDirPath
 
+      
+      // Checking if git workspace is clean
+      this.gitopsConfigHasChanges = false
+      const gitStatus = await this.utils.cliStream('git', ["status", "--porcelain", upath.join(this.clusterConfigDirPath, "config")], {maxBuffer: 300_000_000})
+      if (gitStatus.stdout != "") {
+        this.gitopsConfigHasChanges = true
+      } 
+
     }
     // let clusterConfigDirPath = path.join(found, '..')
     // let yamllintConfigPath = path.join(clusterConfigDirPath, 'yamllint.yaml')
-
-
-
   }
 }
