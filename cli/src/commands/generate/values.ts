003a40f35434df485ae5576ee605d6e2483e24e2
dparbhakar <6168102+dparbhakar@users.noreply.github.com>
2022-12-11 22:02:04 +0100

    cli/commands: Updated generate:values to work with reworked layers

--- cli/src/commands/generate/values.ts
+++ cli/src/commands/generate/values.ts
@@ -10,7 +10,40 @@ export default class GenerateValues extends BaseCommand {
 
   static examples = [`$ kube-core generate values`];
 
-  static flags = {};
+  static flags = {
+    base: Flags.boolean({
+      description:
+        "Generates core, cluster and local helmfiles to allow using kube-core platform. Enabled by default. Use --no-core to disable.",
+      hidden: false,
+      required: false,
+      allowNo: true,
+      default: false,
+    }),
+    full: Flags.boolean({
+      description:
+        "Generates core, cluster and local helmfiles to allow using kube-core platform. Enabled by default. Use --no-core to disable.",
+      hidden: false,
+      required: false,
+      allowNo: true,
+      default: false,
+    }),
+    quickstart: Flags.boolean({
+      description:
+        "Generates core, cluster and local helmfiles to allow using kube-core platform. Enabled by default. Use --no-core to disable.",
+      hidden: false,
+      required: false,
+      allowNo: true,
+      default: false,
+    }),
+    envs: Flags.boolean({
+      description:
+        "Generates core, cluster and local helmfiles to allow using kube-core platform. Enabled by default. Use --no-core to disable.",
+      hidden: false,
+      required: false,
+      allowNo: true,
+      default: false,
+    }),
+  };
 
   static args = [];
 
@@ -18,17 +51,42 @@ export default class GenerateValues extends BaseCommand {
     const { args, argv, flags } = await this.parse(GenerateValues);
     // await this.utils.runCoreScript("commands/generate/values.sh", argv);
 
+    let layersTemplate = "core/layers/base"
+
+    if (flags.quickstart === true) {
+      layersTemplate = "core/layers/generate/quickstart"
+    } else {
+      layersTemplate = "core/layers/generate/full"
+    }
+
+    await this.mergeCoreLayersWithLocalLayers(`${layersTemplate}/values/cluster/config`, `cluster/config`);
+    await this.mergeCoreLayersWithLocalLayers(`${layersTemplate}/values/cluster/releases`, `cluster/releases`);
+
+    await this.mergeCoreLayersWithLocalLayers(`${layersTemplate}/values/core/cluster`, `core/cluster`);
+    await this.mergeCoreLayersWithLocalLayers(`${layersTemplate}/values/core/config`, `core/config`);
+    await this.mergeCoreLayersWithLocalLayers(`${layersTemplate}/values/core/releases`, `core/releases`);
+
+    await this.mergeCoreLayersWithLocalLayers(`${layersTemplate}/values/platform/config`, `platform/config`);
+    await this.mergeCoreLayersWithLocalLayers(`${layersTemplate}/values/platform/environments`, `platform/environments`);
+    await this.mergeCoreLayersWithLocalLayers(`${layersTemplate}/values/platform/applications`, `platform/applications`);
+    await this.mergeCoreLayersWithLocalLayers(`${layersTemplate}/values/platform/services`, `platform/services`);
+
+    await this.mergeCoreLayersWithLocalLayers(`${layersTemplate}/values/envs/default/applications`, `envs/default/applications`);
+    await this.mergeCoreLayersWithLocalLayers(`${layersTemplate}/values/envs/default/services`, `envs/default/services`);
+
+    await this.mergeCoreLayersWithLocalLayers(`${layersTemplate}/values/envs/dev/applications`, `envs/dev/applications`);
+    await this.mergeCoreLayersWithLocalLayers(`${layersTemplate}/values/envs/dev/services`, `envs/dev/services`);
+
+  }
+
+
+  async mergeCoreLayersWithLocalLayers(sourceFolder, targetFolder) {
+
     // Getting layers from core
-    let releaseFiles = await this.utils.getFilesAsList(
-      upath.join(this.corePath, "core/layers/releases")
-    );
     let configFiles = await this.utils.getFilesAsList(
-      upath.join(this.corePath, "core/layers/config")
+      upath.join(this.corePath, sourceFolder)
     );
-    let platformFiles = await this.utils.getFilesAsList(
-      upath.join(this.corePath, "core/layers/generate/platform")
-    );
-    // console.log(files)
+
 
     const localConfigDir = upath.join(this.clusterConfigDirPath, "values");
     const localValuesPath = this.valuesPath
@@ -37,88 +95,53 @@ export default class GenerateValues extends BaseCommand {
       let { name, ext } = upath.parse(file);
       let targetPath = upath.join(
         localConfigDir,
-        "core/config",
+        targetFolder,
         `${name}${ext}`
       );
-      let coreFileData = YAML.parse(await fs.readFile(file, "utf8"));
+      let coreFileData
+      if (ext === ".yaml") {
+        coreFileData = YAML.parse(await fs.readFile(file, "utf8"));
+      } else if (ext === ".gotmpl") {
+        coreFileData = await fs.readFile(file, "utf8")
+      }
       let mergedData = {};
 
       // If layer exists locally, we merge both
       if (this.utils.fileExists(targetPath)) {
         // console.log(`File exists: ${targetPath}`)
-        let currentFileData = YAML.parse(await fs.readFile(targetPath, "utf8"));
-        console.info(
-          `Merging: kube-core:${file.replace(
-            this.corePath,
-            ""
-          )}\t<- local:${targetPath.replace(this.clusterConfigDirPath, "")}`
-        );
-        mergedData = merge(coreFileData, currentFileData);
-        // console.log(mergedData)
+
+        if(ext === ".yaml") {
+          let currentFileData = YAML.parse(await fs.readFile(targetPath, "utf8"));
+          console.info(
+            `Merging: kube-core:${file.replace(
+              this.corePath,
+              ""
+              )}\t<- local:${targetPath.replace(this.clusterConfigDirPath, "")}`
+              );
+              mergedData = merge(coreFileData, currentFileData);
+              // console.log(mergedData)
+        } else if (ext === ".gotmpl") {
+          let currentFileData = await fs.readFile(targetPath, "utf8")
+          console.info(
+            `Copying: kube-core:${file.replace(
+              this.corePath,
+              ""
+              )}\t<- local:${targetPath.replace(this.clusterConfigDirPath, "")}`
+              );
+          mergedData = currentFileData
+        }
       } else {
         mergedData = coreFileData;
       }
       await this.utils.mkdir(upath.parse(targetPath).dir);
-      await fs.writeFile(targetPath, YAML.stringify(mergedData));
-    }
-
-    for (let file of releaseFiles) {
-      let { name, ext } = upath.parse(file);
-      let targetPath = upath.join(
-        localConfigDir,
-        "core/releases",
-        `${name}${ext}`
-      );
-      let coreFileData = YAML.parse(await fs.readFile(file, "utf8"));
-      let mergedData = {};
-
-      // If layer exists locally, we merge both
-      if (this.utils.fileExists(targetPath)) {
-        // console.log(`File exists: ${targetPath}`)
-        let currentFileData = YAML.parse(await fs.readFile(targetPath, "utf8"));
-        console.info(
-          `Merging: kube-core:${file.replace(
-            this.corePath,
-            ""
-          )}\t<- local:${targetPath.replace(this.clusterConfigDirPath, "")}`
-        );
-        mergedData = merge(coreFileData, currentFileData);
-        // console.log(mergedData)
-      } else {
-        mergedData = coreFileData;
+      if(ext === ".yaml") {
+        await fs.writeFile(targetPath, YAML.stringify(mergedData))
+      } else if (ext === ".gotmpl") {
+        await fs.writeFile(targetPath, mergedData)
       }
-      await this.utils.mkdir(upath.parse(targetPath).dir);
-      await fs.writeFile(targetPath, YAML.stringify(mergedData));
-    }
-
-    for (let file of platformFiles) {
-      let { name, ext } = upath.parse(file);
-      let targetPath = upath.join(
-        localValuesPath,
-        "platform",
-        `${name}${ext}`
-      );
-      let coreFileData = YAML.parse(await fs.readFile(file, "utf8"));
-      let mergedData = {};
-
-      // If layer exists locally, we merge both
-      if (this.utils.fileExists(targetPath)) {
-        // console.log(`File exists: ${targetPath}`)
-        let currentFileData = YAML.parse(await fs.readFile(targetPath, "utf8"));
-        console.info(
-          `Merging: kube-core:${file.replace(
-            this.corePath,
-            ""
-          )}\t<- local:${targetPath.replace(this.clusterConfigDirPath, "")}`
-        );
-        mergedData = merge(coreFileData, currentFileData);
-        // console.log(mergedData)
-      } else {
-        mergedData = coreFileData;
-      }
-      await this.utils.mkdir(upath.parse(targetPath).dir);
-      await fs.writeFile(targetPath, YAML.stringify(mergedData));
     }
 
   }
+
+
 }
