e10638d6a546fd59b88593a3eed6e590155e69d3
dparbhakar <6168102+dparbhakar@users.noreply.github.com>
2023-04-29 03:02:40 +0200

    cli/scripts: Added flux.kube-core.io annotation to rewrite flux imagepolicies after kubectl slice

--- cli/scripts/src/gitops/overlays/apply.sh
+++ cli/scripts/src/gitops/overlays/apply.sh
@@ -117,4 +117,15 @@ else
 fi
 fi
 
+if [[ "${run_flux_overlays}" == "true" ]]; then
+    log_info "Applying Flux Overlays..."
+
+    filesList=$(grep -Rl flux.kube-core.io ${configPath} | grep -v ingress-access-operator)
+
+    while read file; do
+        yq -P -i -M '.spec.template.spec.containers[].image line_comment="{\"$imagepolicy\": \""+.metadata.annotations["flux.kube-core.io/imagepolicy"]+"\"}"' $file
+    done <<< "${filesList}"
+fi
+
+
 log_info "Done Applying Overlays!"
